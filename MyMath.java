6b4b0c08c31ecf2214f662b3baa2dc22a22e9775
Lizzy Hamaoka <lizzy.hamaoka@gmail.com>
2018-09-19 23:07:03 -0600

    bubble sort

--- MyMath.java
+++ MyMath.java
@@ -8,8 +8,18 @@ package program;
  */
 public class MyMath implements MathSort {
 
-	public MyMath() {
-		// TODO Auto-generated constructor stub
-	}
+	public void MyMath(int iArray[]) {
+		
+		int n = iArray.length;
+        int temp = 0;
 
-}
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 1; j < n-i-1; j++) {
+                if (iArray[j] > iArray[j+1]) {
+                    temp = iArray[j];
+                    iArray[j] = iArray[j+1];
+                    iArray[j+1] = temp;
+                }
+	}
+            
+           
